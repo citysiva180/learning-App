e8b0ebf4ee8ba8833c506e50be22bd3f1def9b84
Siva Rajan <citysiva180@gmail.com>
2021-11-10 15:13:07 +0530

    practice app with router added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import HomePage from "./components/HomePage/HomePage";
+import AboutPage from "./components/AboutPage/AboutPage";
+import Error from "./components/NotFound/NotFound";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route component={<Error />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Check Out this Home Page</h1>
+    </div>
+  );
+}
+
+export default HomePage;
