ff373186e3b3cd514008b7c1ecefe233730a4ee8
Siva Rajan <citysiva180@gmail.com>
2021-11-18 16:32:27 +0530

    why is this not commiting?

--- src/components/redux/configureStore.js
+++ src/components/redux/configureStore.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootreducer from "./reducers";
 import reduxImmutableStatevariant from "redux-immutable-state-invariant";
-
+import thunk from "redux-thunk";
 export default function configureStore(initialState) {
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__ || compose; // add support for redux dev tools
   return createStore(
     rootreducer,
     initialState,
-    composeEnhancers(applyMiddleware(reduxImmutableStatevariant()))
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStatevariant()))
   );
 }
